aea828986a6697d80841fed5bb45ec0d5cc278fc
Egor Aralov <earalov@thumbtack.net>
2015-06-11 01:35:49 +0600

    pass position

--- BuildingThemes/DetoursHolder.cs
+++ BuildingThemes/DetoursHolder.cs
@@ -12,17 +12,20 @@ namespace BuildingThemes
     class DetoursHolder
     {
 
-        //we'll use this variable to pass block to GetRandomBuildingInfo method
-        private static ushort blockId; //TODO(earalov): maybe ZoneBlock
+        //we'll use this variable to pass position to GetRandomBuildingInfo method. Or we can just pass District
+        private static Vector3 position;
         
 
         //this is detoured version of BuildingManger#GetRandomBuildingInfo method. Note, that it's an instance method. It's better because this way all registers will be expected to have the same values
         //as in original methods
         public BuildingInfo GetRandomBuildingInfo(ref Randomizer r, ItemClass.Service service, ItemClass.SubService subService, ItemClass.Level level, int width, int length, BuildingInfo.ZoningMode zoningMode)
         {
-            Debug.Log("Building Themes: Detoured GetRandomBuildingInfo was called.");
+            //Uncomment this to see that this method is really called (floods output_log.txt)
+            //Debug.Log("Building Themes: Detoured GetRandomBuildingInfo was called.");
+            
             //this part is the same as in original method
-            var buildingManager = (BuildingManager)Convert.ChangeType(this, typeof(BuildingManager)); //I love this hack :)
+            //I love this hack :) This is how I get this reference - and save it to a variable
+            var buildingManager = (BuildingManager)Convert.ChangeType(this, typeof(BuildingManager));  
             var refreshAreaBuidlings = typeof(BuildingManager).GetMethod("RefreshAreaBuildings", BindingFlags.NonPublic|BindingFlags.Instance);
             refreshAreaBuidlings.Invoke(buildingManager, new object[]{});
             var areaBuildings = (FastList<ushort>[])GetInstanceField(typeof(BuildingManager), buildingManager,
@@ -35,7 +38,7 @@ namespace BuildingThemes
             if (fastList.m_size == 0)
                 return (BuildingInfo)null;
            
-            //TODO(earalov): here fastList variable should be filtered. All buildings that  don't belong to block's district should be removed
+            //TODO(earalov): here fastList variable should be filtered. All buildings that  don't belong to district should be removed from tjis list.
             int index = r.Int32((uint)fastList.m_size);
             return PrefabCollection<BuildingInfo>.GetPrefab((uint)fastList.m_buffer[index]);
         }
