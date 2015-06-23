91ec65fcdd4c24868b0705ecd6ba1018b32cffdb
boformer <felix.free@gmx.de>
2015-06-23 22:42:07 +0200

    after hours of random errors. IT WORKS!

--- BuildingThemes/BuildingThemesMod.cs
+++ BuildingThemes/BuildingThemesMod.cs
@@ -101,8 +101,6 @@ namespace BuildingThemes
             get { return "Create building themes and apply them to cities and districts."; }
         }
 
-        private UIButton tab;
-
         public override void OnCreated(ILoading loading)
         {
             base.OnCreated(loading);
@@ -138,17 +136,13 @@ namespace BuildingThemes
 
             // neccessary because of how the game logic is bound to the GUI
 
-            DetoursHolder.refreshPanel = typeof(PoliciesPanel).GetMethod("RefreshPanel", BindingFlags.Instance | BindingFlags.NonPublic);
-            DetoursHolder.refreshPanelState = RedirectionHelper.RedirectCalls(
-                DetoursHolder.refreshPanel,
-                typeof(DetoursHolder).GetMethod("PoliciesPanelRefreshPanel", BindingFlags.Instance | BindingFlags.Public)
-                );
+            PoliciesPanelDetour.refreshPanel = typeof(PoliciesPanel).GetMethod("RefreshPanel", BindingFlags.Instance | BindingFlags.NonPublic);
+            PoliciesPanelDetour.refreshPanelDetour = typeof(PoliciesPanelDetour).GetMethod("RefreshPanel", BindingFlags.Instance | BindingFlags.NonPublic);
+            PoliciesPanelDetour.refreshPanelState = RedirectionHelper.RedirectCalls(PoliciesPanelDetour.refreshPanel, PoliciesPanelDetour.refreshPanelDetour);
 
-            DetoursHolder.setParentButton = typeof(PoliciesPanel).GetMethod("SetParentButton", BindingFlags.Instance | BindingFlags.Public);
-            DetoursHolder.setParentButtonState = RedirectionHelper.RedirectCalls(
-                DetoursHolder.setParentButton,
-                typeof(DetoursHolder).GetMethod("PoliciesPanelSetParentButton", BindingFlags.Instance | BindingFlags.Public)
-                );
+            PoliciesPanelDetour.setParentButton = typeof(PoliciesPanel).GetMethod("SetParentButton", BindingFlags.Instance | BindingFlags.Public);
+            PoliciesPanelDetour.setParentButtonDetour = typeof(PoliciesPanelDetour).GetMethod("SetParentButton", BindingFlags.Instance | BindingFlags.Public);
+            PoliciesPanelDetour.setParentButtonState = RedirectionHelper.RedirectCalls(PoliciesPanelDetour.setParentButton, PoliciesPanelDetour.setParentButtonDetour);
 
             if (isDebug)
             {
@@ -156,16 +150,11 @@ namespace BuildingThemes
             }
         }
 
-        private Boolean initializedGUI = false;
-
         public override void OnLevelLoaded(LoadMode mode) 
         {
             // Is it an actual game ?
-            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame) return;
+            //if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame) return;
 
-            initializedGUI = true;
-
-            AddThemesTab();
             // TODO load data (serialized for policies, xml for themes)
 
             // Hook into policies GUI
@@ -174,27 +163,21 @@ namespace BuildingThemes
 
         public override void OnLevelUnloading()
         {
-            if (!initializedGUI) return;
-
-            // Remove the custom policy tab
-            RemoveThemesTab();
             Singleton<BuildingThemesManager>.instance.Reset();
-
-            ToolsModifierControl.policiesPanel.component.eventVisibilityChanged -= OnPoliciesPanelVisibilityChanged;
         }
 
         public override void OnReleased() 
         {
+            UnityEngine.Debug.LogFormat("Reverting detoured methods...");
+            
             RedirectionHelper.RevertRedirect(DetoursHolder.getRandomBuildingInfo, DetoursHolder.getRandomBuildingInfoState);
             RedirectionHelper.RevertJumpTo(DetoursHolder.zoneBlockSimulationStepPtr, DetoursHolder.zoneBlockSimulationStepState);
             RedirectionHelper.RevertJumpTo(DetoursHolder.resourceManagerAddResourcePtr, DetoursHolder.resourceManagerAddResourceState);
 
             // GUI
-            RedirectionHelper.RevertRedirect(DetoursHolder.refreshPanel, DetoursHolder.refreshPanelState);
-            RedirectionHelper.RevertRedirect(DetoursHolder.setParentButton, DetoursHolder.setParentButtonState);
+            RedirectionHelper.RevertRedirect(PoliciesPanelDetour.refreshPanel, PoliciesPanelDetour.refreshPanelState);
+            RedirectionHelper.RevertRedirect(PoliciesPanelDetour.setParentButton, PoliciesPanelDetour.setParentButtonState);
         }
-
-
 
         private string GetCurrentEnvironment()
         {
@@ -202,159 +185,6 @@ namespace BuildingThemes
         }
 
         // GUI stuff
-
-        private void OnPoliciesPanelVisibilityChanged(UIComponent component, bool visible)
-        {
-            // It is necessary to remove the custom tab when the panel is closed 
-            // because the game logic is coupled to the GUI
-            if (visible)
-            {
-                AddThemesTab();
-            }
-            else
-            {
-                RemoveThemesTab();
-            }
-        }
-
-        private void AddThemesTab()
-        {
-            UITabstrip tabstrip = ToolsModifierControl.policiesPanel.Find("Tabstrip") as UITabstrip;
-            tab = tabstrip.AddTab("Themes");
-            tab.stringUserData = "CityPlanning";
-
-            // recalculate the width of the tabs
-            for (int i = 0; i < tabstrip.tabCount; i++)
-            {
-                tabstrip.tabs[i].width = tabstrip.width / ((float)tabstrip.tabCount - 1);
-            }
-
-            // TODO this is hacky. better store it in a field
-            GameObject go = GameObject.Find("Tab 5 - Themes");
-            if (go == null)
-            {
-                return;
-            }
-
-            // remove the default stuff if something is in there
-            foreach (Transform child in go.transform)
-            {
-                GameObject.Destroy(child.gameObject);
-            }
-
-            UIPanel container = go.GetComponent<UIPanel>();
-
-            container.autoLayout = true;
-            container.autoLayoutDirection = LayoutDirection.Vertical;
-            container.autoLayoutPadding.top = 5;
-            
-            container.isVisible = tabstrip.selectedIndex == 4;
-
-            foreach (Configuration.Theme theme in Singleton<BuildingThemesManager>.instance.GetAllThemes())
-            {
-                AddThemePolicyButton(container, theme);
-            }
-        }
-
-        private void RemoveThemesTab()
-        {
-            // TODO this is hacky. better store it in a field
-            GameObject go = GameObject.Find("Tab 5 - Themes");
-            if (go == null)
-            {
-                return;
-            }
-            GameObject.Destroy(go);
-
-            UITabstrip tabstrip = ToolsModifierControl.policiesPanel.Find("Tabstrip") as UITabstrip;
-            tabstrip.RemoveUIComponent(tab);
-            GameObject.Destroy(tab.gameObject);
-        }
-
-        private void AddThemePolicyButton(UIPanel container, Configuration.Theme theme)
-        {
-
-            UIPanel policyPanel = container.AddUIComponent<UIPanel>();
-            policyPanel.name = theme.name;
-            policyPanel.backgroundSprite = "GenericPanel";
-            policyPanel.size = new Vector2(364f, 44f);
-            policyPanel.objectUserData = ToolsModifierControl.policiesPanel;
-            policyPanel.stringUserData = "None";
-
-            UIButton policyButton = policyPanel.AddUIComponent<UIButton>();
-            policyButton.name = "PolicyButton";
-            policyButton.text = theme.name;
-            policyButton.size = new Vector2(324f, 40f);
-            policyButton.focusedBgSprite = "PolicyBarBackActive";
-            policyButton.normalBgSprite = "PolicyBarBack";
-            policyButton.relativePosition = new Vector3(2f, 2f, 0f);
-            policyButton.textPadding.left = 50;
-            policyButton.textColor = new Color32(0, 0, 0, 255);
-            policyButton.disabledTextColor = new Color32(0, 0, 0, 255);
-            policyButton.hoveredTextColor = new Color32(0, 0, 0, 255);
-            policyButton.pressedTextColor = new Color32(0, 0, 0, 255);
-            policyButton.focusedTextColor = new Color32(0, 0, 0, 255);
-            policyButton.disabledColor = new Color32(124, 124, 124, 255);
-            policyButton.dropShadowColor = new Color32(103, 103, 103, 255);
-            policyButton.dropShadowOffset = new Vector2(1f, 1f);
-            policyButton.textHorizontalAlignment = UIHorizontalAlignment.Left;
-            policyButton.useDropShadow = false;
-            policyButton.textScale = 0.875f;
-            policyButton.gameObject.AddComponent<ThemePolicyContainer>();
-
-            UICheckBox policyCheckBox = policyButton.AddUIComponent<UICheckBox>();
-            policyCheckBox.name = "Checkbox";
-            policyCheckBox.size = new Vector2(363f, 44f);
-            policyCheckBox.relativePosition = new Vector3(0f, -2f, 0f);
-            policyCheckBox.clipChildren = true;
-            policyCheckBox.objectUserData = theme;
-
-            ushort districtId1 = (ushort)ToolsModifierControl.policiesPanel.targetDistrict;
-
-            var districtThemes = Singleton<BuildingThemesManager>.instance.GetDistrictThemes(districtId1, true);
-            policyCheckBox.isChecked = districtThemes.Contains(theme);
-
-
-            policyCheckBox.eventCheckChanged += delegate(UIComponent component, bool enabled)
-            {
-                lock (component)
-                {
-                    uint districtId = (uint)ToolsModifierControl.policiesPanel.targetDistrict;
-                    if (enabled)
-                    {
-                        Singleton<BuildingThemesManager>.instance.EnableTheme(districtId, theme, true);
-                        if (BuildingThemesMod.isDebug)
-                        {
-                            Debug.Log("enabled theme " + theme.name + " in district " + districtId);
-                        }
-                    }
-                    else
-                    {
-                        Singleton<BuildingThemesManager>.instance.DisableTheme(districtId, theme.name, true);
-                        if (BuildingThemesMod.isDebug)
-                        {
-                            Debug.Log("disabled theme " + theme.name + " in district " + districtId);
-                        }
-                    } 
-                }
-
-            };
-
-
-            UISprite sprite = policyCheckBox.AddUIComponent<UISprite>();
-            sprite.name = "Unchecked";
-            sprite.spriteName = "ToggleBase";
-            sprite.size = new Vector2(16f, 16f);
-            sprite.relativePosition = new Vector3(336.6984f, 14, 0f);
-
-            policyCheckBox.checkedBoxObject = sprite.AddUIComponent<UISprite>();
-            policyCheckBox.checkedBoxObject.name = "Checked";
-            ((UISprite)policyCheckBox.checkedBoxObject).spriteName = "ToggleBaseFocused";
-            policyCheckBox.checkedBoxObject.size = new Vector2(16f, 16f);
-            policyCheckBox.checkedBoxObject.relativePosition = Vector3.zero;
-
-            // TODO link the checkbox and the focus of the button (like PolicyContainer component does)
-        }
 
     }
 }
