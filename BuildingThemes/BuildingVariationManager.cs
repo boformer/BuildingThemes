a4e005dc896964374d07f3a54c6458e52888d2f2
boformer <felix.free@gmx.de>
2015-07-11 19:27:33 +0200

    BetterUpgrade prefab cloning

---
+++ BuildingThemes/BuildingVariationManager.cs
@@ -0,0 +1,71 @@
+﻿using ColossalFramework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuildingThemes
+{
+    public class BuildingVariationManager : Singleton<BuildingVariationManager>
+    {
+        // TODO store lists of variation names per theme
+        // TODO upgrade mapping
+        public Dictionary<string, BuildingInfo> createVariations(BuildingInfo prefab) 
+        {
+            var prefabLevel = (int)prefab.m_class.m_level + 1;
+            var maxAllowedLevel = getMaxAllowedLevel(prefab.m_class);
+
+            var prefabVariations = new Dictionary<string, BuildingInfo>();
+            
+            foreach (var theme in BuildingThemesManager.instance.GetAllThemes()) 
+            {
+                var building = theme.getBuilding(prefab.name); // TODO add support for service type variation?
+
+                var minLevel = building.minLevel < 1 ? prefabLevel : Mathf.Min(building.minLevel, maxAllowedLevel);
+                var maxLevel = Mathf.Clamp(building.maxLevel < 1 ? prefabLevel : building.maxLevel, minLevel, maxAllowedLevel);
+
+                for(int l = minLevel; l <= maxLevel; l++) 
+                {
+                    var level = (ItemClass.Level)(l - 1);
+
+                    if (level == prefab.m_class.m_level) continue;
+                    
+                    var variationName = prefab.name + "#" + level;
+
+                    if(prefabVariations.ContainsKey(variationName)) continue;
+
+                    var variationClass = ScriptableObject.CreateInstance<ItemClass>();
+
+                    variationClass.m_layer = prefab.m_class.m_layer;
+                    variationClass.m_service = prefab.m_class.m_service;
+                    variationClass.m_subService = prefab.m_class.m_subService;
+                    variationClass.m_level = level;
+
+                    BuildingInfo prefabVariation = BuildingInfo.Instantiate(prefab);
+                    prefabVariation.name = variationName;
+                    prefabVariation.m_class = variationClass;
+
+                    prefabVariations.Add(variationName, prefabVariation);
+                }
+            }
+
+            return prefabVariations;
+        }
+
+        private int getMaxAllowedLevel(ItemClass itemClass) 
+        {
+            switch (itemClass.m_service) 
+            { 
+                case ItemClass.Service.Residential:
+                    return 5;
+                case ItemClass.Service.Commercial:
+                    return 3;
+                case ItemClass.Service.Office:
+                    return 3;
+                case ItemClass.Service.Industrial:
+                    if(itemClass.m_subService == ItemClass.SubService.IndustrialGeneric) return 3;
+                    goto default;
+                default:
+                    return 1;
+            }
+        }
+    }
+}
---
+++ BuildingThemes/Detour/BuildingInfoDetour.cs
@@ -0,0 +1,71 @@
+﻿using ColossalFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BuildingThemes.Detour
+{
+    public class BuildingInfoDetour : BuildingInfo
+    {
+        private static bool deployed = false;
+        
+        private static RedirectCallsState _InitializePrefab_state;
+        private static MethodInfo _InitializePrefab_original;
+        private static MethodInfo _InitializePrefab_detour;
+
+        public static void Deploy() 
+        {
+            if (!deployed)
+            {
+                _InitializePrefab_original = typeof(BuildingInfo).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
+                _InitializePrefab_detour = typeof(BuildingInfoDetour).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
+                _InitializePrefab_state = RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
+
+                deployed = true;
+
+                Debugger.Log("Building Themes: BuildingInfo Methods detoured!");
+            }
+        }
+
+        public static void Revert() 
+        {
+            if (deployed)
+            {
+                RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
+                _InitializePrefab_original = null;
+                _InitializePrefab_detour = null;
+
+                deployed = false;
+
+                Debugger.Log("Better Themes: BuildingInfo Methods restored!");
+            }
+        }
+
+        public new virtual void InitializePrefab()
+        {
+            bool growable = this.m_class.GetZone() != ItemClass.Zone.None;
+
+            if (growable)
+            {
+                //Debugger.Log("InitializePrefab called: " + this.name);
+            }
+
+            RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
+            base.InitializePrefab();
+            RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
+
+            if (growable) 
+            {
+                var prefabVariations = Singleton<BuildingVariationManager>.instance.createVariations(this).Values.ToArray<BuildingInfo>();
+
+                if (prefabVariations.Length > 0) 
+                { 
+                    PrefabCollection<BuildingInfo>.InitializePrefabs("BetterUpgrade", prefabVariations, null);
+                }
+                //Debugger.Log("InitializePrefab done:   " + this.name);
+            }
+        }
+    }
+}
