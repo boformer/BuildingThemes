758f151cd2e73eec17108b530d3237e45e527def
Egor Aralov <earalov@thumbtack.net>
2015-09-23 04:05:59 +0600

    Refs #11 Add new categories to Theme Manager UI. Closes #11

--- BuildingThemes/GUI/ThemeManager/CategoryUtils.cs
+++ BuildingThemes/GUI/ThemeManager/CategoryUtils.cs
@@ -27,6 +27,8 @@ namespace BuildingThemes.GUI
         ResidentialHigh,
         CommercialLow,
         CommercialHigh,
+        CommercialLeisure,
+        CommercialTourism,
         Industrial,
         Farming,
         Forestry,
@@ -38,17 +40,21 @@ namespace BuildingThemes.GUI
     public class CategoryIcons
     {
 
-        public static readonly string[] atlases = { "Thumbnails", "Thumbnails", "Thumbnails", "Thumbnails", "Thumbnails",
+        public static readonly string[] atlases = { "Thumbnails", "Thumbnails", "Thumbnails", "Thumbnails", "Ingame", "Ingame", "Thumbnails",
                                                        "Ingame", "Ingame", "Ingame", "Ingame", "Thumbnails" };
 
-        public static readonly string[] spriteNames = { "ZoningResidentialLow", "ZoningResidentialHigh", "ZoningCommercialLow", "ZoningCommercialHigh", "ZoningIndustrial",
-                                                      "IconPolicyFarming", "IconPolicyForest", "IconPolicyOil", "IconPolicyOre", "ZoningOffice" };
+        public static readonly string[] spriteNames = { "ZoningResidentialLow", "ZoningResidentialHigh", "ZoningCommercialLow", "ZoningCommercialHigh",
+                                                        "IconPolicyLeisure", "IconPolicyTourist",
+                                                        "ZoningIndustrial", "IconPolicyFarming", "IconPolicyForest", "IconPolicyOil", "IconPolicyOre",
+                                                        "ZoningOffice" };
 
         public static readonly string[] tooltips = {
             "Low density residential",
             "High density residential",
             "Low density commercial",
             "High density commercial",
+            "Leisure commercial",
+            "Tourism commercial",
             "Generic Industry",
             "Farming Industry",
             "Forest Industry",
