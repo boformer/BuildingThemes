96b9aa3354326ba4d09450bea389931b83e9876c
earalov <earalov@thumbtack.net>
2016-11-12 01:06:46 +0600

    Invoke Prefab Hook compatibility API for compatibility with Prefab Hook

--- BuildingThemes/Detour/BuildingInfoDetour.cs
+++ BuildingThemes/Detour/BuildingInfoDetour.cs
@@ -10,33 +10,65 @@ namespace BuildingThemes.Detour
     public class BuildingInfoDetour : BuildingInfo
     {
         private static bool deployed = false;
-        
+        private static bool isGrowable = false;
+        private static BuildingInfo previousPrefab = null;
+
         private static RedirectCallsState _InitializePrefab_state;
         private static MethodInfo _InitializePrefab_original;
         private static MethodInfo _InitializePrefab_detour;
 
-        public static void Deploy() 
+        public static void Deploy()
         {
             if (!deployed)
             {
-                _InitializePrefab_original = typeof(BuildingInfo).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
-                _InitializePrefab_detour = typeof(BuildingInfoDetour).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
-                _InitializePrefab_state = RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
-
+                if (Util.IsModActive("Prefab Hook"))
+                {
+                    var type = Util.FindType("BuildingInfoHookReflective");
+                    type.GetMethod("RegisterPreInitializationHook", BindingFlags.Public | BindingFlags.Static)
+                        .Invoke(null, new object[]
+                        {
+                            new Action<BuildingInfo>(PreInitializeHook), 
+                        });
+                    type.GetMethod("RegisterPostInitializationHook", BindingFlags.Public | BindingFlags.Static)
+                        .Invoke(null, new object[]
+                        {
+                            new Action<BuildingInfo>(PostInitializeHook),
+                        });
+                    type.GetMethod("Deploy", BindingFlags.Public | BindingFlags.Static)
+                        .Invoke(null, new object[]
+                        {
+                        });
+                }
+                else
+                {
+                    _InitializePrefab_original = typeof(BuildingInfo).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
+                    _InitializePrefab_detour = typeof(BuildingInfoDetour).GetMethod("InitializePrefab", BindingFlags.Instance | BindingFlags.Public);
+                    _InitializePrefab_state = RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
+                }
                 deployed = true;
 
                 Debugger.Log("Building Themes: BuildingInfo Methods detoured!");
             }
         }
 
-        public static void Revert() 
+        public static void Revert()
         {
             if (deployed)
             {
-                RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
-                _InitializePrefab_original = null;
-                _InitializePrefab_detour = null;
-
+                if (Util.IsModActive("Prefab Hook"))
+                {
+                    var type = Util.FindType("BuildingInfoHookReflective");
+                    type.GetMethod("Revert", BindingFlags.Public | BindingFlags.Static)
+                        .Invoke(null, new object[]
+                        {
+                        });
+                }
+                else
+                {
+                    RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
+                    _InitializePrefab_original = null;
+                    _InitializePrefab_detour = null;
+                }
                 deployed = false;
 
                 Debugger.Log("Better Themes: BuildingInfo Methods restored!");
@@ -45,27 +77,63 @@ namespace BuildingThemes.Detour
 
         public new virtual void InitializePrefab()
         {
-            bool growable = this.m_class.GetZone() != ItemClass.Zone.None;
+            try
+            {
+                PreInitializeHook(this);
+            }
+            catch
+            {
+                isGrowable = false;
+                previousPrefab = null;
+            }
+            RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
+            try
+            {
+                base.InitializePrefab();
+            }
+            finally
+            {
+                RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
+            }
+            try
+            {
+                PostInitializeHook(this);
+            }
+            catch(Exception e)
+            {
+                UnityEngine.Debug.LogException(e);
+            }
 
+        }
+
+        private static void PreInitializeHook(BuildingInfo prefab)
+        {
+
+            var growable = prefab.m_class.GetZone() != ItemClass.Zone.None;
             if (growable)
             {
                 //Debugger.Log("InitializePrefab called: " + this.name);
             }
+            isGrowable = growable;
+            previousPrefab = prefab;
 
-            RedirectionHelper.RevertRedirect(_InitializePrefab_original, _InitializePrefab_state);
-            base.InitializePrefab();
-            RedirectionHelper.RedirectCalls(_InitializePrefab_original, _InitializePrefab_detour);
 
-            if (growable) 
+        }
+
+        private static void PostInitializeHook(BuildingInfo prefab)
+        {
+
+            if (!isGrowable || prefab != previousPrefab)
             {
-                var prefabVariations = Singleton<BuildingVariationManager>.instance.CreateVariations(this).Values.ToArray<BuildingInfo>();
-
-                if (prefabVariations.Length > 0) 
-                { 
-                    PrefabCollection<BuildingInfo>.InitializePrefabs("BetterUpgrade", prefabVariations, null);
-                }
-                //Debugger.Log("InitializePrefab done:   " + this.name);
+                return;
             }
+            var prefabVariations = Singleton<BuildingVariationManager>.instance.CreateVariations(prefab).Values.ToArray<BuildingInfo>();
+
+            if (prefabVariations.Length > 0)
+            {
+                PrefabCollection<BuildingInfo>.InitializePrefabs("BetterUpgrade", prefabVariations, null);
+            }
+            //Debugger.Log("InitializePrefab done:   " + this.name);
         }
     }
 }
--- BuildingThemes/Util.cs
+++ BuildingThemes/Util.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ColossalFramework.Plugins;
 using ICities;
 
@@ -15,6 +16,26 @@ namespace BuildingThemes
                     select instances[0].Name into name
                     where name == modName
                     select name).Any();
+        }
+
+        public static Type FindType(string className)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    var types = assembly.GetTypes();
+                    foreach (var type in types.Where(type => type.Name == className))
+                    {
+                        return type;
+                    }
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            return null;
         }
     }
 }
